a494afe4e32e253ea1e6d5d4e89b878046e4eaad
Dawid Jeż <dawidjez@gmail.com>
2024-02-18 10:37:07 +0100

    Create AuthController and update middleware settings
    Implemented the AuthController to manage user login and logout processes. The required routes were added to handle these functionalities. Also updated the middleware settings in the Kernel file to simplify the references. Changes were made in the initialization script as well to introduce environment setup and key generation.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use OpenApi\Annotations as OA;
+/**
+ * @OA\Info(
+ *   title="TODO API",
+ *   version="1.0.0",
+ *   description="TODO API",
+ *   @OA\Contact(
+ *     email="adres@email.com",
+ *     name="Imię Nazwisko"
+ *   )
+ * )
+ */
+
+class AuthController extends Controller
+{
+    /**
+     * @OA\Post(
+     *      path="/api/login",
+     *      operationId="login",
+     *      tags={"Auth"},
+     *      summary="Login user",
+     *      description="Token",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="email",
+     *                      type="string"
+     *                  ),
+     *                  @OA\Property(
+     *                      property="password",
+     *                      type="string"
+     *                  ),
+     *                  example={"email": "user@domain.com", "password": "password123"}
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken('api-token')->plainTextToken;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json('Logged out successfully', 200);
+    }
+
+}
--- init-app.sh
+++ init-app.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
+
+cp ./env.example ./.env
+php artisan key:generate
+
 #install dependencies
 composer install
 
 #create secret
-php artisan jwt:secret
